4eaa343862132ca52cf93938d5ea794830bca9b1
prince <phiphat.s@jasmine.com>
2022-09-16 16:42:10 +0700

    mock up design

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,15 @@ import type { NextPage } from 'next'
 
 
 const Home: NextPage = () => {
+  const control = 'flex h-screen items-center justify-center text-white snap-start'
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      1
+    <div className='snap-mandatory snap-y h-screen overflow-y-scroll'>
+      <section className={`${control} bg-yellow-200`} >
+        1
+      </section>
+      <section className={`${control} bg-slate-400`}>
+        2
+      </section>
     </div>
   )
 }
