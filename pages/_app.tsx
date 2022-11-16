c35482d7bbedee5b48269b8fd65783c8922c61a0
prince <phiphat.s@jasmine.com>
2022-11-16 16:50:56 +0700

    card flip

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Whan Prince</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Sarabun:wght@300;400;500;600&display=swap" rel="stylesheet"></link>
       </Head>
       <Component {...pageProps} />
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,36 @@
-import type { NextPage } from 'next'
+import Image from 'next/image'
 
-
-const Home: NextPage = () => {
+const Home = () => {
   const control = 'flex h-screen items-center justify-center text-white snap-start'
   return (
-    <div className='snap-mandatory snap-y h-screen overflow-y-scroll'>
-      <section className={`${control} bg-yellow-200`} >
-        1
-      </section>
+    <div className="h-screen snap-y snap-mandatory overflow-y-scroll">
       <section className={`${control} bg-slate-400`}>
-        2
+        <div className="flex min-h-full flex-col justify-center">
+          <div className="group h-128 w-96 [perspective:1000px]">
+            <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
+              <div className="absolute inset-0">
+                <Image
+                  className=" h-full w-full rounded-xl object-cover"
+                  src="/card_font.jpg"
+                  alt="Picture of the author"
+                  width={500}
+                  height={700}
+                ></Image>
+              </div>
+              <div className="absolute inset-0 h-full w-full rounded-xl [transform:rotateY(180deg)] [backface-visibility:hidden]">
+                <Image
+                  className="h-full w-full rounded-xl object-cover"
+                  src="/card_back.jpg"
+                  alt="Picture of the author"
+                  width={500}
+                  height={700}
+                ></Image>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
+      <section className={`${control} bg-yellow-200`}>2222222222</section>
     </div>
   )
 }
